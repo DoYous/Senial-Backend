075fda574f1c9a748e69fe46fac53c2c587fc561
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-12-05 00:33:38 +0900

    취미 게시판 취미 후기 삭제 Rest api 기능 구현 #27

--- src/main/java/com/senials/hobbyreview/service/HobbyReviewService.java
+++ src/main/java/com/senials/hobbyreview/service/HobbyReviewService.java
@@ -18,20 +18,20 @@ public class HobbyReviewService {
     private final HobbyRepository hobbyRepository;
     private final UserRepository userRepository;
 
-    HobbyReviewService(HobbyReviewRepository hobbyReviewRepository, HobbyReviewMapper hobbyReviewMapper, HobbyRepository hobbyRepository, UserRepository userRepository){
-        this.hobbyReviewMapper=hobbyReviewMapper;
-        this.hobbyReviewRepository=hobbyReviewRepository;
-        this.hobbyRepository=hobbyRepository;
-        this.userRepository=userRepository;
+    HobbyReviewService(HobbyReviewRepository hobbyReviewRepository, HobbyReviewMapper hobbyReviewMapper, HobbyRepository hobbyRepository, UserRepository userRepository) {
+        this.hobbyReviewMapper = hobbyReviewMapper;
+        this.hobbyReviewRepository = hobbyReviewRepository;
+        this.hobbyRepository = hobbyRepository;
+        this.userRepository = userRepository;
     }
 
     //취미 번호가 같은 취미리뷰 리스트 불러오기
     public List<HobbyReviewDTO> getReviewsListByHobbyNumber(int hobbyNumber) {
         Hobby hobby = hobbyRepository.findById(hobbyNumber)
                 .orElseThrow(() -> new IllegalArgumentException("해당 취미 번호가 존재하지 않습니다: " + hobbyNumber));
-        List<HobbyReview> hobbyReviewList=hobbyReviewRepository.findByHobby(hobby);
-        List<HobbyReviewDTO> hobbyReviewDTOList=hobbyReviewList.stream().map(hobbyReview -> {
-            HobbyReviewDTO dto=hobbyReviewMapper.toHobbyReviewDTO(hobbyReview);
+        List<HobbyReview> hobbyReviewList = hobbyReviewRepository.findByHobby(hobby);
+        List<HobbyReviewDTO> hobbyReviewDTOList = hobbyReviewList.stream().map(hobbyReview -> {
+            HobbyReviewDTO dto = hobbyReviewMapper.toHobbyReviewDTO(hobbyReview);
             dto.setHobbyNumber(hobbyReview.getHobby().getHobbyNumber());
             dto.setUserNumber(hobbyReview.getUser().getUserNumber());
             dto.setUserName(hobbyReview.getUser().getUserName());
@@ -41,18 +41,19 @@ public class HobbyReviewService {
     }
 
     //취미번호, 유저번호, 리뷰번호를 통한 대조후 취미리뷰 조회
-    public HobbyReviewDTO getHobbyReview(int hobbyNumber, int userNumber, int hobbyReviewNumber){
-        User user= (User) userRepository.findById(userNumber).orElseThrow(() -> new IllegalArgumentException("해당 유저 번호가 존재하지 않습니다: " + userNumber));;
-        Hobby hobby=hobbyRepository.findById(hobbyNumber).orElseThrow(() -> new IllegalArgumentException("해당 취미 번호가 존재하지 않습니다: " + hobbyNumber));
+    public HobbyReviewDTO getHobbyReview(int hobbyNumber, int userNumber, int hobbyReviewNumber) {
+        User user = (User) userRepository.findById(userNumber).orElseThrow(() -> new IllegalArgumentException("해당 유저 번호가 존재하지 않습니다: " + userNumber));
+        ;
+        Hobby hobby = hobbyRepository.findById(hobbyNumber).orElseThrow(() -> new IllegalArgumentException("해당 취미 번호가 존재하지 않습니다: " + hobbyNumber));
         HobbyReview hobbyReview = hobbyReviewRepository.findById(hobbyReviewNumber).orElseThrow(() -> new IllegalArgumentException("해당 리뷰 번호가 존재하지 않습니다: " + hobbyReviewNumber));
-        if(!hobbyReview.getUser().equals(user)){
+        if (!hobbyReview.getUser().equals(user)) {
             throw new IllegalArgumentException("해당 유저의 리뷰가 아닙니다.");
         }
         if
         (!hobbyReview.getHobby().equals(hobby)) {
             throw new IllegalArgumentException("해당 취미의 리뷰가 아닙니다.");
         }
-        HobbyReviewDTO hobbyReviewDTO=hobbyReviewMapper.toHobbyReviewDTO(hobbyReview);
+        HobbyReviewDTO hobbyReviewDTO = hobbyReviewMapper.toHobbyReviewDTO(hobbyReview);
         hobbyReviewDTO.setHobbyNumber(hobby.getHobbyNumber());
         hobbyReviewDTO.setUserNumber(user.getUserNumber());
         hobbyReviewDTO.setUserName(hobbyReview.getUser().getUserName());
@@ -62,13 +63,32 @@ public class HobbyReviewService {
 
     //취미 번호와 유저 번호를 통해 취미후기를 생성
     public HobbyReview saveHobbyReview(HobbyReviewDTO hobbyReviewDTO, int userNumber, int hobbyNumber) {
-        User user= userRepository.findById(userNumber).orElseThrow(() -> new IllegalArgumentException("해당 유저 번호가 존재하지 않습니다: " + userNumber));;
-        Hobby hobby=hobbyRepository.findById(hobbyNumber).orElseThrow(() -> new IllegalArgumentException("해당 취미 번호가 존재하지 않습니다: " + hobbyNumber));
+        User user = userRepository.findById(userNumber).orElseThrow(() -> new IllegalArgumentException("해당 유저 번호가 존재하지 않습니다: " + userNumber));
+        ;
+        Hobby hobby = hobbyRepository.findById(hobbyNumber).orElseThrow(() -> new IllegalArgumentException("해당 취미 번호가 존재하지 않습니다: " + hobbyNumber));
         HobbyReview hobbyReviewEntity = hobbyReviewMapper.toHobbyReviewEntity(hobbyReviewDTO);
         hobbyReviewEntity.InitializeUser(user);
         hobbyReviewEntity.InitializeHobby(hobby);
-        HobbyReview hobbyReview=hobbyReviewRepository.save(hobbyReviewEntity);
+        HobbyReview hobbyReview = hobbyReviewRepository.save(hobbyReviewEntity);
 
         return hobbyReview;
     }
+
+    //취미번호, 유저번호, 리뷰번호를 통한 대조후 취미리뷰 삭제
+    public void deleteHobbyReview(int hobbyReviewNumber, int userNumber, int hobbyNumber){
+        User user= userRepository.findById(userNumber).orElseThrow(() -> new IllegalArgumentException("해당 유저 번호가 존재하지 않습니다: " + userNumber));;
+        Hobby hobby=hobbyRepository.findById(hobbyNumber).orElseThrow(() -> new IllegalArgumentException("해당 취미 번호가 존재하지 않습니다: " + hobbyNumber));
+        HobbyReview hobbyReview = hobbyReviewRepository.findById(hobbyReviewNumber).orElseThrow(() -> new IllegalArgumentException("해당 리뷰 번호가 존재하지 않습니다: " + hobbyReviewNumber));
+        if(!hobbyReview.getUser().equals(user)){
+            throw new IllegalArgumentException("해당 유저의 리뷰가 아닙니다.");
+        }
+        if
+        (!hobbyReview.getHobby().equals(hobby)) {
+            throw new IllegalArgumentException("해당 취미의 리뷰가 아닙니다.");
+        }
+
+        hobbyReviewRepository.delete(hobbyReview);
+    }
+
 }
+
