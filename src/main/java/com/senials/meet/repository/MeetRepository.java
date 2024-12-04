1535c3e6aff931636bcac07203529ef4ca9e3906
min-g126 <qoalstj01@gmail.com>
2024-12-04 23:19:44 +0900

    회원별 모임 일정 조회 Rest api 기능 구현

--- src/main/java/com/senials/meet/repository/MeetRepository.java
+++ src/main/java/com/senials/meet/repository/MeetRepository.java
@@ -2,6 +2,13 @@ package com.senials.meet.repository;
 
 import com.senials.meet.entity.Meet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MeetRepository extends JpaRepository<Meet, Integer> {
+
+    //사용자 별 참여한 모임 확인
+    @Query("SELECT m FROM Meet m JOIN m.partyBoard pb WHERE pb.user.userNumber = :userNumber")
+    List<Meet> findAllByUserNumber(int userNumber);
 }
---
+++ src/main/java/com/senials/meet/service/MeetService.java
@@ -0,0 +1,34 @@
+package com.senials.meet.service;
+
+import com.senials.meet.MeetDTO.MeetDTO;
+import com.senials.meet.entity.Meet;
+import com.senials.meet.repository.MeetRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MeetService {
+    private final MeetRepository meetRepository;
+
+    public MeetService(MeetRepository meetRepository) {
+        this.meetRepository = meetRepository;
+    }
+
+    public List<MeetDTO> getMeetsByUserNumber(int userNumber) {
+        List<Meet> meets = meetRepository.findAllByUserNumber(userNumber);
+        return meets.stream()
+                .map(meet -> new MeetDTO(
+                        meet.getMeetNumber(),
+                        meet.getMeetStartDate(),
+                        meet.getMeetEndDate(),
+                        meet.getMeetStartTime(),
+                        meet.getMeetFinishTime(),
+                        meet.getMeetEntryFee(),
+                        meet.getMeetLocation(),
+                        meet.getMeetMaxMember()
+                ))
+                .collect(Collectors.toList());
+    }
+}
