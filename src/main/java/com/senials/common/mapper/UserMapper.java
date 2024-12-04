660b95b1fd93d05986772cc3aea4a5b40d2a0f33
do0ik <oulwind00@naver.com>
2024-12-04 11:42:25 +0900

    Initial Commit 1. com.senials.example 패키지명을 바꿔서 그대로 사용하지 말고 복사해 새로운 패키지로 사용하시기 바랍니다.

---
+++ src/main/java/com/senials/common/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.senials.common.mapper;
+
+import com.senials.example.dto.UserDTO;
+import com.senials.example.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+
+    // User -> UserDTO
+    UserDTO toUserDTO(User user);
+
+}
