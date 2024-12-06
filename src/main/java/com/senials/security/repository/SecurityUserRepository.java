b8126509cca31fbeba8d231fd23e03f6c52556fd
Ahnhyojun <hyojunahn111@naver.com>
2024-12-07 03:32:31 +0900

    이름 수정

--- src/main/java/com/senials/security/repository/SecurityUserRepository.java
+++ src/main/java/com/senials/security/repository/SecurityUserRepository.java
@@ -3,6 +3,6 @@ package com.senials.security.repository;
 import com.senials.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SecurityUserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+public interface SecurityUserRepository extends JpaRepository<User, Integer> {
+    User findByUserName(String userName); // 메서드 이름 수정
 }
