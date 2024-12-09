9dc258f22a2ff16c6a80fe570145be76cef8e863
do0ik <oulwind00@naver.com>
2024-12-10 04:53:46 +0900

    [Issue #124] 모임 대표 이미지 저장 로직 개선

--- src/main/java/com/senials/partyboard/dto/PartyBoardDTOForWrite.java
+++ src/main/java/com/senials/partyboard/dto/PartyBoardDTOForWrite.java
@@ -5,8 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.List;
-
 @NoArgsConstructor
 @Getter
 @Setter
@@ -19,16 +17,9 @@ public class PartyBoardDTOForWrite {
 
     private String partyBoardDetail;
 
-    // 이미지 임시 저장 경로 번호
-    private String tempNumber;
-
-    // 이미지 이름 리스트
-    private List<String> savedFiles;
-
-    public PartyBoardDTOForWrite(int hobbyNumber, String partyBoardName, String partyBoardDetail, List<String> savedFiles) {
+    public PartyBoardDTOForWrite(int hobbyNumber, String partyBoardName, String partyBoardDetail) {
         this.hobbyNumber = hobbyNumber;
         this.partyBoardName = partyBoardName;
         this.partyBoardDetail = partyBoardDetail;
-        this.savedFiles = savedFiles;
     }
 }
