36c5d1463a973ee475ae7928cbffe644bf0dcbc9
Ahnhyojun <hyojunahn111@naver.com>
2024-12-13 10:39:38 +0900

    #137 사용되지 않는 젝슨 비활성화

--- src/main/java/com/senials/security/config/JacksonConfig.java
+++ src/main/java/com/senials/security/config/JacksonConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.senials.security.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,4 +15,4 @@ public class JacksonConfig {
         objectMapper.registerModule(new JavaTimeModule()); // JSR310 모듈 등록
         return objectMapper;
     }
-}
+}*/
