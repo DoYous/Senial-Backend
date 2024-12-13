5b23671d28ef0b96edb764dc5f3523e139cc1d5b
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-12-13 09:16:12 +0900

    hobbyCard dto 추가 #140

--- src/main/java/com/senials/common/mapper/HobbyMapper.java
+++ src/main/java/com/senials/common/mapper/HobbyMapper.java
@@ -1,5 +1,6 @@
 package com.senials.common.mapper;
 
+import com.senials.hobbyboard.dto.HobbyDTOForCard;
 import com.senials.hobbyboard.entity.Hobby;
 import com.senials.hobbyboard.dto.HobbyDTO;
 import org.mapstruct.Mapper;
@@ -7,5 +8,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface HobbyMapper {
     HobbyDTO toHobbyDTO(Hobby hobby);
+
+    HobbyDTOForCard toHobbyDTOForCard(Hobby hobby);
 
 }
---
+++ src/main/java/com/senials/hobbyboard/dto/HobbyDTOForCard.java
@@ -0,0 +1,9 @@
+package com.senials.hobbyboard.dto;
+
+import lombok.*;
+public class HobbyDTOForCard {
+
+    private int hobbyNumber;       // 취미 번호
+    private String hobbyName;      // 취미 이름
+    private String hobbyImg;       // 취미 이미지 경로
+}
